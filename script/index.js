54bf4c46a467dd596276ca6bd323622e6c976ba8
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-20 21:10:41 +0600

    module-27-6

--- script/index.js
+++ script/index.js
@@ -1,7 +1,16 @@
-function play() {
-    // step-1 hide the home screen. To hide the screen add the class hidden to the home section.
+// function play() {
+//     // step-1 hide the home screen. To hide the screen add the class hidden to the home section.
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden')
+//     // show the playground
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden')
+//     // console.log(playGroundSection.classList)
 
-    // Show the Playground.
-    const homeSection = document.getElementById('home-screen');
-    homeSection.classList.add('hidden')
+// }
+
+function play() {
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
+
